8749dce5cc475e56dc3da179cef36d4f25cd6f42
Jeremy Darling <jeremy.darling@gmail.com>
2015-06-23 10:58:35 -0500

    More hapi 6 and 8 support fixes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-var HapiCapture = require('./lib/hapi-capture');
+var Hapi6Capture = require('./lib/hapi6-capture');
+var Hapi8Capture = require('./lib/hapi8-capture');
 var HttpCapture = require('./lib/http-capture');
 
 module.exports = {
   Logger: require('./lib/logger').Logger,
-  HAPICapture: HapiCapture.capture,
-  HAPIRegister: HapiCapture.register,
+  HAPI6Capture: Hapi6Capture.capture,
+  HAPI6Register: Hapi6Capture.register,
+  HAPI8Capture: Hapi8Capture.capture,
+  HAPI8Register: Hapi8Capture.register,
+  HAPICapture: Hapi8Capture.capture,
+  HAPIRegister: Hapi8Capture.register,
   HTTPCapture: HttpCapture.capture,
   captureUtils: require('./lib/capture-utils'),
 };
